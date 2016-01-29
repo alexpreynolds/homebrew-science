17ebbb21217f22ad92162e5de2275dbf98916575
Alex Reynolds <areynolds@stamlab.org>
2016-01-29 14:51:01 -0800

    follow-up to https://github.com/Homebrew/homebrew-science/pull/3266#discussion_r51325748

--- sample.rb
+++ sample.rb
@@ -1,4 +1,5 @@
 class Sample < Formula
+  desc "This tool performs reservoir sampling (Vitter, 'Random sampling with a reservoir'; cf. http://dx.doi.org/10.1145/3147.3165 and also: http://en.wikipedia.org/wiki/Reservoir_sampling) with or without replacement on very large text files that are delimited by newline characters (such as common genomic data formats like BED, SAM, VCF, etc.) and scales to larger inputs than GNU shuf by sampling line offsets, instead of whole lines."
   homepage "https://github.com/alexpreynolds/sample"
   # tag "bioinformatics"
 
@@ -7,13 +8,6 @@ class Sample < Formula
 
   head "https://github.com/alexpreynolds/sample.git"
   version "1.0.2"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "4c5a2dfda0ea2658bc505ef5bdf47a199b810fe36f20a8fffe275395afc1cc80" => :el_capitan
-    sha256 "5faad7689e3ced8019f9f9833f629f581237922753fa8ff3785f8e23c5f48eab" => :yosemite
-    sha256 "18f3d8dbcd1bb202eb913c7238e5249dcf0ea98996306ecedbe090d2d56e5e91" => :mavericks
-  end
 
   env :std
 
